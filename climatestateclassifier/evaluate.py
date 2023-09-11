3df6975b633ee874d7ae131ed57bade6fcdf04e3
Johannes Meuer <jomeuer@googlemail.com>
2023-09-11 14:28:14 +0200

    make data root dir list

--- climatestateclassifier/evaluate.py
+++ climatestateclassifier/evaluate.py
@@ -129,8 +129,8 @@ def evaluate(arg_file=None, prog_func=None):
                                       eval_name="average_{}".format(cfg.eval_names[i_model]))
 
             if cfg.plot_prediction_overview:
-                plot_prediction_overview(outputs, labels, eval_name="{}".format(cfg.eval_names[i_model]))
-                plot_class_predictions(outputs, labels, eval_name="{}".format(cfg.eval_names[i_model]))
+                #plot_prediction_overview(outputs, labels, eval_name="{}".format(cfg.eval_names[i_model]))
+                #plot_class_predictions(outputs, labels, eval_name="{}".format(cfg.eval_names[i_model]))
                 plot_predictions_by_category(outputs, labels, categories, eval_name="{}".format(cfg.eval_names[i_model]))
                 #plot_predictions_by_category_graph(outputs, categories, eval_name="{}".format(cfg.eval_names[i_model]))
             if cfg.plot_single_predictions:
